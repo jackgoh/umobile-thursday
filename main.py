c128bf9ed9c7c5596e92eed2b45581dee858f7c4
jack <jack@botahead.com>
2017-12-30 23:35:30 +0800

    moved redeem outside nested loop added option parser added redeem state added sleep if error

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import sys
 import json
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
+from optparse import OptionParser
 
-state = False
+# init
 campaign_id = None
 promo_voucher = None
+redeem_state = True
 headers={"Host": "mobileapps.u.com.my",
 "Accept-Encoding": "br, gzip, deflate",
 "Connection": "keep-alive",
@@ -19,34 +21,47 @@ headers={"Host": "mobileapps.u.com.my",
 
 session = requests.session()
 
-while True:
+# parser 
+parser = OptionParser()
+parser.add_option("-n", "--number", dest="phone_number",
+                  help="Your Umobile Phone Number without +6")
+(options, args) = parser.parse_args()
+
+if not options.phone_number:
+    print('Error: Run with -n. Eg: python main.py -n 0181234567')
+    sys.exit()
+
+phone_num = options.phone_number
+
+while redeem_state:
     try:
         # get vouchers 
-        res = session.get("https://mobileapps.u.com.my/prdcampaign/api/top-deals.php?msisdn=60180000000&is_u_special=false", headers=headers)
+        res = session.get("https://mobileapps.u.com.my/prdcampaign/api/top-deals.php?msisdn=6"+ str(phone_num) +"&is_u_special=false", headers=headers)
         data = json.loads(res.text)
 
         # loop through all vouchers 
         for voucher in data['vouchers']:
-
             # find voucher that is thursday promo 
-            if vouchers['is_thursday_promo'] == True and vouchers['is_thursday_active'] == True:
+            if voucher['is_thursday_promo'] == True and voucher['is_thursday_active'] == True:
                 campaign_id = voucher['campaign_id']
                 promo_voucher = voucher 
             
-            # check if promo voucher exist 
-            if campaign_id == '0' or not campaign_id:
-                print('not yet')
+        campaign_id = 820
+        # check if promo voucher exist 
+        if campaign_id == '0' or not campaign_id:
+            print('not yet')
+        else:
+            # redeem vouchers
+            res = session.get("https://mobileapps.u.com.my/prdcampaign/api/assign-voucher.php?msisdn=6"+ str(phone_num) +"&campaign_id="+ str(campaign_id) +"&plan=21", headers=headers)
+            redeem_data = json.loads(res.text)
+
+            if res.status_code == 200:
+                print('sucess', promo_voucher)
             else:
-                # redeem vouchers
-                res = session.get("https://mobileapps.u.com.my/prdcampaign/api/assign-voucher.php?msisdn=asd&campaign_id="+ str(campaign_id) +"&plan=21", headers=headers)
-                redeem_data = json.loads(res.text)
+                print('failed', redeem_data)
 
-                if res.status_code == 200:
-                    print('sucess', promo_voucher)
-                    break
-                else:
-                    print('failed', redeem_data)
+            redeem_state = False
 
-    except:
-        time.sleep(1)
-        print('request error, probably timeout')
+    except Exception as e:
+        time.sleep(2)
+        print(e)
